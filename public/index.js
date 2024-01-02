d4605f6734cfe0d43686a288f3abc4f3d2d240e1
Jack Dutton <duttonjack@gmail.com>
2024-01-02 09:32:04 -0700

    Implemented event listeners on colored boxes to allow toggling from red to green indicating if the task is completed or not

--- public/index.js
+++ public/index.js
@@ -6,10 +6,15 @@ fetch("/list")
     for (item of data){
     const $entryDiv = $('<div class="entry"><div>')
     const $boxDiv = $('<div class="box"></div>')
+    $boxDiv.on('click', function(event){
+        if ($boxDiv.attr("id") === "box1"){
+            $boxDiv.attr("id", "box2")
+        } else {
+            $boxDiv.attr("id", "box1")
+        }
+    })
     const $taskDiv = $("<p class='task'>").text(item.descr)
-    
-    $entryDiv.append($boxDiv)
-    $entryDiv.append($taskDiv)
+    $entryDiv.append($boxDiv, $taskDiv)
     $listContainer.append($entryDiv)
     }
   })
