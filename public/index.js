d8f25c86bf1172f994cb03fd8fc3497247504e47
Jack Dutton <duttonjack@gmail.com>
2024-01-02 11:16:12 -0700

    implemented POST functionality where a user can add a new entry to the list and the list displayed on the page will update accordingly.

--- public/index.js
+++ public/index.js
@@ -1,31 +1,62 @@
 const $listContainer = $('#listContainer')
+const $inputText = $("#inputArea")
+
+function updateList() {
+    fetch('/list')
+        .then(res => {
+            if (!res.ok){
+                throw new Error('HTTP Error in upDate List')
+            }
+            return res.json()
+        })
+        .then(data => {
+            $listContainer.empty();
+            for (item of data){
+                const $entryDiv = $('<div class="entry"><div>')
+                const $boxDiv = $('<div class="box"></div>')
+                $boxDiv.on('click', function(event){
+                    if ($boxDiv.attr("id") === "box1"){
+                        $boxDiv.attr("id", "box2")
+                    } else {
+                        $boxDiv.attr("id", "box1")
+                    }
+                })
+                const $taskDiv = $("<p class='task'>").text(item.descr)
+                $entryDiv.append($boxDiv, $taskDiv)
+                $listContainer.append($entryDiv)
+                }        
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error)
+        })
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     // Code that runs after the DOM is fully loaded
+    updateList()
 });
 
 // Event Listeners
-
+$("#addButton").on('click', function(event){
+    // fetch request to server
+    const input = $inputText.val().trim()
+    console.log("text input:", typeof input, input)
+    fetch('/list', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: input })
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data, data.message)
+        updateList();
+    })
+    .catch(error => {
+        console.error('Error adding to list: ', error)
+    })
+    // update page so new item displays
+})
 
 // Fetch Blocks
-fetch("/list")
-  .then((res) => res.json())
-  .then((data) => {
-    for (item of data){
-    const $entryDiv = $('<div class="entry"><div>')
-    const $boxDiv = $('<div class="box"></div>')
-    $boxDiv.on('click', function(event){
-        if ($boxDiv.attr("id") === "box1"){
-            $boxDiv.attr("id", "box2")
-        } else {
-            $boxDiv.attr("id", "box1")
-        }
-    })
-    const $taskDiv = $("<p class='task'>").text(item.descr)
-    $entryDiv.append($boxDiv, $taskDiv)
-    $listContainer.append($entryDiv)
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error)
-  })
--- server.js
+++ server.js
@@ -34,6 +34,19 @@ app.get('/list', (req, res) => {
     })
 })
 
+app.post('/list', (req, res) => {
+    console.log("made it into server Post route")
+    const inputText = req.body.text
+    console.log(inputText)
+    pool.query('INSERT INTO list (descr) VALUES ($1)', [ inputText ])
+    .then((result) => {
+        res.status(200).json("success")
+    })
+    .catch((error) => {
+        res.status(500).json("Post Server Error")
+    })
+})
+
 app.listen(expressPort, () => {
     console.log(`Server listening at port ${expressPort}`)
 })
